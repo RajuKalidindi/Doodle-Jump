d069f0a29451c1b1a2627474c769c96340cbaed5
RajuKalidindi <krkraju997@gmail.com>
2018-08-31 00:17:47 +0530

    first commit

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    public GameObject platformPrefab;
+    public GameObject specialPrefab;
+
+    public int numberOfPlatforms = 10;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+	// Use this for initialization
+	void Start () {
+        Vector3 spawnPosition = new Vector3();
+        spawnPosition.y += .4f;
+        spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+        Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        for (int i = 1; i < numberOfPlatforms; i++)
+        {
+            if (i % 100 == 0)
+            {
+                spawnPosition.y += Random.Range(minY, maxY);
+                spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+                Instantiate(specialPrefab, spawnPosition, Quaternion.identity);
+            }
+            else
+            {
+                spawnPosition.y += Random.Range(minY, maxY);
+                spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+                Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            }          
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform target;
+    public Text scoreText;
+    private float game;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.transform.position = new Vector3(0f, target.position.y + 4f, 0f);
+        scoreText.text = (target.position.y * 10).ToString("0") ;
+    }
+}
